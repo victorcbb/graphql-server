ef8e891654588ad34e7d27cd91408fd8db477b6c
Victor <victorclovisb@hotmail.com>
2025-08-27 16:25:35 -0300

    feat: Run server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('oipas');
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world! 🚀',
+  },
+};
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server);
+
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+startApolloServer();
