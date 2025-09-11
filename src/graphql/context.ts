3a302f2b71e3cfa33a742e36a3c4f65fb9156f8f
Victor <victorclovisb@hotmail.com>
2025-09-11 10:25:07 -0300

    feat: Add getPosts resolver and update post GraphQL type to accept ID parameter

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -2,6 +2,7 @@ import { api } from '../lib/axios.ts';
 
 export const context = async () => {
   return {
-    getUsers: (id: string = '/') => api(`/users/${id}`),
+    getUsers: (id: string = '/') => api(`/users${id}`),
+    getPosts: (id: string = '/') => api(`/posts${id}`),
   };
 };
--- src/graphql/post/resolvers.ts
+++ src/graphql/post/resolvers.ts
@@ -1,25 +1,26 @@
-const post = () => {
-  return {
-    id: '1',
-    title: 'Entendendo Graphql',
-  };
+import { type AxiosResponse } from 'axios';
+
+const post = async (
+  _parent: any,
+  arg: { id: string },
+  context: { getPosts: (_id?: string) => Promise<AxiosResponse<any, any>> },
+) => {
+  const { getPosts } = context;
+  const { id } = arg;
+
+  const response = await getPosts(`/${id}`);
+  return response.data;
 };
 
-const posts = () => {
-  return [
-    {
-      id: '1',
-      title: 'Entendendo Graphql',
-    },
-    {
-      id: '2',
-      title: 'Conhecendo Apollo Server',
-    },
-    {
-      id: '3',
-      title: 'Como criar queries e resolvers',
-    },
-  ];
+const posts = async (
+  _parent: any,
+  _arg: { id: string },
+  context: { getPosts: (_id?: string) => Promise<AxiosResponse<any, any>> },
+) => {
+  const { getPosts } = context;
+
+  const response = await getPosts();
+  return response.data;
 };
 
 export const postResolvers = {
